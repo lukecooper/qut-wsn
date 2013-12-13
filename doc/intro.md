813fd3e61d94978d4568ee7caff6fb6a6e018855
lukecooper <luke.cooper@gmail.com>
2013-12-13 22:24:48 +1000

    Added pewson skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pewson
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject pewson "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot pewson.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
