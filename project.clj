a7c745b57b20dd0dd079d46175b132716019f22c
lukecooper <luke.cooper@gmail.com>
2014-01-16 16:43:56 +1000

    More progress on task definition and execution

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.rmoquin.bundle/jeromq "0.2.0"]
-                 [cheshire "5.2.0"]]
-  :main pewson.core
+                 [cheshire "5.2.0"]
+                 [me.raynes/conch "0.5.0"]]
+  :Main pewson.core
   :profiles {:uberjar {:aot :all}})
--- src/pewson/core.clj
+++ src/pewson/core.clj
@@ -4,105 +4,31 @@
   (:require (cheshire [core :as c]))
   (:use [clojure.java.shell :only [sh]]))
 
-(def ctx (ZMQ/context 1))
- 
-;
-; The Request-Reply Pattern
-;
-; Server
-(defn echo-server
-  []
-  (let [s (.socket ctx ZMQ/REP)]
-    (.bind s "tcp://debianvm.local:5554")
-    (loop [msg (String. (.recv s))]
-      (.send s (pr-str (load-string msg)))
-      (recur (String. (.recv s))))))
- 
-; Client
-(defn echo
-  [msg]
-  (let [s (.socket ctx ZMQ/REQ)]
-    (.connect s "tcp://debianvm.local:5554")
-    (.send s msg)
-    (println "Server replied:" (String. (.recv s)))
-    (.close s)))
- 
-;
-; The Publish-Subscribe Pattern
-;
-; Server
-(defn market-data-publisher
-  []
-  (let [s (.socket ctx ZMQ/PUB)
-        market-data-event (fn []
-                            {:symbol (rand-nth ["CAT" "UTX"])
-                             :size (rand-int 1000)
-                             :price (format "%.2f" (rand 50.0))})]
-    (.bind s "tcp://127.0.01:6666")
-    (while :true
-      (.send s (c/generate-string (market-data-event))))))
- 
-; Client
-(defn get-market-data
-  [num-events]
-  (let [s (.socket ctx ZMQ/SUB)]
-    (.subscribe s "")
-    (.connect s "tcp://127.0.01:6666")
-    (dotimes [_ num-events]
-      (println (c/parse-string (String. (.recv s)))))
-    (.close s)))
- 
-;
-; The Pipeline Pattern
-;
-; Dispatcher
-(defn dispatcher
-  [jobs]
-  (let [s (.socket ctx ZMQ/PUSH)]
-    (.bind s "tcp://127.0.01:7777")
-    (Thread/sleep 1000)   
-    (dotimes [n jobs]
-      (.send s (str n)))
-    (.close s)))
- 
-; Worker
-(defn worker
-  []
-  (let [rcv (.socket ctx ZMQ/PULL)
-        snd (.socket ctx ZMQ/PUSH)
-        id (str (gensym "w"))]
-    (.connect rcv "tcp://127.0.01:7777")
-    (.connect snd "tcp://127.0.01:8888")
-    (while :true
-      (let [job-id (String. (.recv rcv))
-            proc-time (rand-int 100)]
-        (Thread/sleep proc-time)
-        (.send snd (c/generate-string {:worker-id id
-                                       :job-id job-id
-                                       :processing-time proc-time}))))))
- 
-; Collector
-(defn collector
-  []
-  (let [s (.socket ctx ZMQ/PULL)]
-    (.bind s "tcp://127.0.01:8888")
-    (while :true
-      (->> (.recv s)
-           (String.)
-           (c/parse-string)
-           (println "Job completed:")))))
-
-(defn shell
-  [command]
-  #(apply sh (clojure.string/split command #"\s+")))
-
 (defn ls
   []
-  (:out ((shell "ls -lh"))))
+  (:out (clojure.java.shell/sh "ls" "-lh")))
 
-(defn grep-doc
+(defn grep
   [input]
-  (println (:out (sh "grep" "doc" :in input))))
+  (:out (clojure.java.shell/sh "grep" "src" :in input)))
+
+(def ls-taskdef
+  {:name 'ls
+   :exec ls
+   :repeat false})
+
+(def grep-taskdef
+  {:name 'grep
+   :exec grep
+   :depends 'ls})
+
+(def ctx (ZMQ/context 1))
+
+(defn gen-pubsock
+  []
+  (let [socket (.socket ctx ZMQ/PUB)]
+    (.bind socket "tcp://127.0.0.1:8888")
+    socket))
 
 (defn append-filter
   [filter message]
@@ -112,62 +38,32 @@
   [message]
   (second (clojure.string/split message #":" 2)))
 
-(defn publish-socket
-  []
-  (let [socket (.socket ctx ZMQ/PUB)]
-    (.bind socket "tcp://127.0.0.1:8888")
-    socket))
-
 (defn publish
-  [socket filter result]
-  (.send socket (append-filter filter (ls))))
+  [socket message filter]
+  (.send socket (append-filter filter message)))
 
-(defmacro subscribe
-  [filter task]
-  `(let [socket# (.socket ctx ZMQ/SUB)]
-     (.connect socket# "tcp://127.0.0.1:8888")
-     (.subscribe socket# ~filter)
-     (~task (remove-filter (String. (.recv socket#))))
-     (.close socket#)))
+(defn wait-for
+  [task]
+  (let [socket (.socket ctx ZMQ/SUB)
+        filter (str task)]
+    (.connect socket "tcp://127.0.0.1:8888")
+    (.subscribe socket filter)
+    (let [message (String. (.recv socket))]
+      (.close socket)
+      (remove-filter message))))
 
-(defn ls-task
-  [publisher filter]
-  (.send publisher (append-filter filter (ls))))
+(defn exec-task
+  [task-defn]
+  (if (nil? (:depends task-defn))
+    ((:exec task-defn))
+    (let [input (wait-for (:depends task-defn))]
+      ((:exec task-defn) input))))
 
-(defn print-task
-  [address port filter]
-  (let [subscriber (.socket ctx ZMQ/SUB)]
-    (.connect subscriber (str "tcp://" address ":" port))
-    (.subscribe subscriber filter)
-    (println (remove-filter (String. (.recv subscriber))))
-    (.close subscriber)))
-
-(defmacro tasker
-  ([task]
-     `~task)
-  ([task publish-socket]
-     `(let [result# ~task]
-        (.send ~publish-socket result#)))
-  ([task publish-socket subscribe-socket]
-     `(let [message# (.recv ~subscribe-socket)
-            result# (~reverse (~conj ~task message#))]
-        (.send ~publish-socket result#))))
-
-(defn testpub
-  []
-  (let [pub (.socket ctx ZMQ/PUB)]   
-    (.bind pub "tcp://127.0.01:7772")
-    (tasker (:out (ls)) pub)
-    (.close pub)))
-
-(defn testsub
-  []
-  (let [pub (.socket ctx ZMQ/PUB)
-        sub (.socket ctx ZMQ/SUB)]
-    (.bind pub "tcp://127.0.01:7773")
-    (.subscribe sub "")
-    (.connect sub "tcp://127.0.01:7772")
-    (tasker (println) pub sub)
-    (.close pub)
-    (.close sub)))
-
+(defn run-task  
+  [task-defn pubsock]
+  (loop []
+    (let [result (exec-task task-defn)]
+      (println result)
+      (publish pubsock result (:name task-defn)))
+    (if (:repeat task-defn)
+      (recur))))
